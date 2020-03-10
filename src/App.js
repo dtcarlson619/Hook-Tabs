aafaf370beab5dfc9382c4b9bb9b2503fe65544e
d_carlson_dev <53411336+dtcarlson619@users.noreply.github.com>
2020-03-10 10:40:15 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Tabs from './components/Tabs';
+import Content from './components/Content';
 
 function App() {
+  const [setIndex, setIndexState] = useState(0)
+
+  const changeValue = (textIndex) => {
+    setIndexState(textIndex)
+  }
+
+  const textArray = [
+    "This one is good",
+    "This one is better",
+    "This one is the best"
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Tabs changeValue={() => changeValue(0)} tabLabel="Tab 1" />
+        <Tabs changeValue={() => changeValue(1)} tabLabel="Tab 2" />
+        <Tabs changeValue={() => changeValue(2)} tabLabel="Tab 3" />
+        <Content displayText={textArray[setIndex]} />
     </div>
   );
 }
